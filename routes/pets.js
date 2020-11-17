31498c9273ec02d6dc4a46cbc931c3d4cede0559
MackRoe <elaine.music@students.makeschool.com>
2020-11-16 21:10:54 -0500

    implemented Stripe payment

--- routes/pets.js
+++ routes/pets.js
@@ -1,4 +1,6 @@
 // MODELS
+const Stripe = require('stripe');
+
 const Pet = require('../models/pet');
 
 const mailer = require('../utils/mailer');
@@ -78,8 +80,8 @@ module.exports = (app) => {
         });
     });
 
-  // PURCHASE
-    app.post('/pets/:id/purchase', (req, res) => {
+    // PURCHASE
+  app.post('/pets/:id/purchase', (req, res) => {
     console.log(req.body);
     // Set your secret key: remember to change this to your live secret key in production
     // See your keys here: https://dashboard.stripe.com/account/apikeys
@@ -93,8 +95,8 @@ module.exports = (app) => {
     // this way we'll insure we use a non-null value
     let petId = req.body.petId || req.params.id;
 
-    Pet.findById(petId).exec((err, pet) => {
-      if(err) {
+    Pet.findById(petId).exec((err, pet)=> {
+      if (err) {
         console.log('Error: ' + err);
         res.redirect(`/pets/${req.params.id}`);
       }
@@ -104,20 +106,14 @@ module.exports = (app) => {
         description: `Purchased ${pet.name}, ${pet.species}`,
         source: token,
       }).then((chg) => {
-      // Convert the amount back to dollars for ease in displaying in the template
-        const user = {
-          email: req.body.stripeEmail,
-          amount: chg.amount / 100,
-          petName: pet.name
-        };
-        // Call our mail handler to manage sending emails
-        mailer.sendMail(user, req, res);
+        res.redirect(`/pets/${req.params.id}`);
       })
       .catch(err => {
-        console.log('Error: ' + err);
+        console.log('Error:' + err);
       });
     })
-  });
+  }); 
+
 
   app.post('/pets/:id/purchase', (req,res) => {
       console.log(`purchase body: ${req.body}`);
